9842f9a6672073bc737f39c10aa192f47d7b16d3
Mateen Shahzad <142880053+CodeByMateen@users.noreply.github.com>
2023-11-19 18:33:54 +0500

    Add files via upload

--- Searching Algorithms/binary_search.py
+++ Searching Algorithms/binary_search.py
@@ -2,7 +2,7 @@ def binary_search(arr, target):
     left, right = 0, len(arr) - 1
 
     while left <= right:
-        mid = (left + right) // 2
+        mid = left + (right - left) // 2
         mid_value = arr[mid]
 
         if mid_value == target:
